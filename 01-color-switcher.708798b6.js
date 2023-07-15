cbb3b71b031c43732997b09a047d415696f7797c
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-15 12:06:05 +0000

    Deploying to gh-pages from @ OksanaLesjuk/goit-js-hw-09@0310baba9af7f3199bb8d17269eeee2ffff0a671 🚀

---
+++ 01-color-switcher.708798b6.js
@@ -0,0 +1,2 @@
+console.log("Це тест налаштування проекту ");
+//# sourceMappingURL=01-color-switcher.708798b6.js.map
---
+++ 01-color-switcher.ff56c2ee.js
@@ -0,0 +1,2 @@
+console.log("Це тест налаштування проекту ");
+//# sourceMappingURL=01-color-switcher.ff56c2ee.js.map
