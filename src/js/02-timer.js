433c859e446e631bb01407ed3bdbf3fbe41fbaad
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-15 20:21:25 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,18 +8,41 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
+
+        const chooseDate = selectedDates[0];
+        const currentDate = new Date();
+        console.log(currentDate);
+        const isValidTime = chooseDate > currentDate;
+        if (!isValidTime) {
+            btnStart.disabled = true;
+            alert("Please choose a date in the future");
+        }
+        else {
+            btnStart.disabled = false;
+        }
     },
-};
+}
+
+
 
 const myInput = document.querySelector("#datetime-picker");
 const btnStart = document.querySelector('button[data-start]');
 
+
+btnStart.disabled = true;
+
 const fp = flatpickr(myInput, options);
+console.dir(fp);
 
 
+
+
+// myInput.addEventListener('input', onInput)
 
 btnStart.addEventListener(
     'click', onClickStart)
+
+
 
 function onClickStart(evt) {
     console.log(evt);
